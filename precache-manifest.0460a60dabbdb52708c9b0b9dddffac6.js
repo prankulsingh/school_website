adeaf81484ee4786de6b78e44fa9097d24201b06
Prankul Singh <prankul.singh@airtel.com>
2021-01-25 14:31:54 +0530

    Updates

---
+++ precache-manifest.0460a60dabbdb52708c9b0b9dddffac6.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc62b5642bce0854f4774c23121b2a34",
+    "url": "/school_website/index.html"
+  },
+  {
+    "revision": "b633dd4cd949324a9a3e",
+    "url": "/school_website/static/css/main.0b4ce531.chunk.css"
+  },
+  {
+    "revision": "5b0bc21424841757d328",
+    "url": "/school_website/static/js/2.7b3b90ad.chunk.js"
+  },
+  {
+    "revision": "ac4a31b929cb9380c4a242904540a469",
+    "url": "/school_website/static/js/2.7b3b90ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b633dd4cd949324a9a3e",
+    "url": "/school_website/static/js/main.045b65d8.chunk.js"
+  },
+  {
+    "revision": "73d3c7c4c1d4a35e1a9a",
+    "url": "/school_website/static/js/runtime-main.d82b73fc.js"
+  },
+  {
+    "revision": "7257a1a94be550133f3233fd792ee724",
+    "url": "/school_website/static/media/avatar.7257a1a9.jpg"
+  },
+  {
+    "revision": "6b1983a26a32647c4832456dc9f5dcd3",
+    "url": "/school_website/static/media/bg1.6b1983a2.jpg"
+  },
+  {
+    "revision": "e643a6cf77b4635dda856f10fa9b7db4",
+    "url": "/school_website/static/media/bg11.e643a6cf.jpg"
+  },
+  {
+    "revision": "f807b37b1736e7709918ea35ef80ca7d",
+    "url": "/school_website/static/media/bg3.f807b37b.jpg"
+  },
+  {
+    "revision": "4bff5d45104e2e6c59b1825b54864d58",
+    "url": "/school_website/static/media/bg4.4bff5d45.jpg"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/school_website/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "d7d1aaf71d275992031bd75cdb6ec65c",
+    "url": "/school_website/static/media/bg6.d7d1aaf7.jpg"
+  },
+  {
+    "revision": "607a4a36d058e55773625ba23518e291",
+    "url": "/school_website/static/media/bg7.607a4a36.jpg"
+  },
+  {
+    "revision": "6737ce3b2577a1c3977e4d479511f642",
+    "url": "/school_website/static/media/bg8.6737ce3b.jpg"
+  },
+  {
+    "revision": "f7cb2d390498c2969b2b1050f3e024ce",
+    "url": "/school_website/static/media/creative-tim-white-slim2.f7cb2d39.png"
+  },
+  {
+    "revision": "089c4f4e55f158a69d0364c6b6339f37",
+    "url": "/school_website/static/media/eva.089c4f4e.jpg"
+  },
+  {
+    "revision": "ffcc99c42164b65da7af6307b66379cf",
+    "url": "/school_website/static/media/header.ffcc99c4.jpg"
+  },
+  {
+    "revision": "a76c7b4ce725bb07b1572699a5a78c62",
+    "url": "/school_website/static/media/hero-image-1.a76c7b4c.png"
+  },
+  {
+    "revision": "9616730d099782a3ea6c12c70aa181af",
+    "url": "/school_website/static/media/hero-image-2.9616730d.png"
+  },
+  {
+    "revision": "02cd0e2d2b27926d1ed5c31a00dd26fa",
+    "url": "/school_website/static/media/hero-image-3.02cd0e2d.png"
+  },
+  {
+    "revision": "300494e9857b8d865ead79a62b09dd88",
+    "url": "/school_website/static/media/invision-white-slim.300494e9.png"
+  },
+  {
+    "revision": "138381e51c42397348a82b448b5fb61b",
+    "url": "/school_website/static/media/julie.138381e5.jpg"
+  },
+  {
+    "revision": "f8fba45478e18757e67c37b6644a3cec",
+    "url": "/school_website/static/media/landing.f8fba454.jpg"
+  },
+  {
+    "revision": "b21019516fd574253e2354080dc1dd00",
+    "url": "/school_website/static/media/login.b2101951.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/school_website/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/school_website/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/school_website/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/school_website/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "225653f9f4dacef3e81bcc52f5ef547d",
+    "url": "/school_website/static/media/profile.225653f9.jpg"
+  },
+  {
+    "revision": "f217f346c88e5fb2720ca7c28827ff72",
+    "url": "/school_website/static/media/ryan.f217f346.jpg"
+  }
+]);
