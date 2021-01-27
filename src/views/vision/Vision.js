c8a90a8460301f8454520f0dae9bda6cc3df7b4e
Himanshu Negi <himanshu.negi@curefit.com>
2021-01-27 20:13:38 +0530

    added vision page

--- src/views/vision/Vision.js
+++ src/views/vision/Vision.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { Col, Container } from "reactstrap";
 
 function Vision() {
     return (
-        <>
-            <div>test vision</div>
-        </>
+        <div className="section vision-page">
+            <Container>
+                <h2>Vision</h2>
+                <div className="image-container">
+                    <img src={require("assets/img/bg1.jpg")} alt={"Nature, United States"} />
+                </div>
+                <p>
+                    I will be the leader of a company that ends up being
+                    worth billions of dollars, because I got the answers. I
+                    understand culture. I am the nucleus. I think that’s a
+                    responsibility that I have, to push possibilities, to show
+                    people, this is the level that things could be at.
+                    I will be the leader of a company that ends up being
+                    worth billions of dollars, because I got the answers. I
+                    understand culture. I am the nucleus. I think that’s a
+                    responsibility that I have, to push possibilities, to show
+                    people, this is the level that things could be at.
+                    I will be the leader of a company that ends up being
+                    worth billions of dollars, because I got the answers. I
+                    understand culture. I am the nucleus. I think that’s a
+                    responsibility that I have, to push possibilities, to show
+                    people, this is the level that things could be at.
+                </p>
+            </Container>
+        </div>
     );
 }
 
