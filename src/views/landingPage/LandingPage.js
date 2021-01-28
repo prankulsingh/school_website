0c0e1b42c07c05590221416253d67372d6865f5d
Himanshu Negi <himanshu.negi@curefit.com>
2021-01-28 23:05:14 +0530

    cleanup

--- src/views/landingPage/LandingPage.js
+++ src/views/landingPage/LandingPage.js
@@ -21,20 +21,6 @@ import './LandingPage.scss'
 import * as Constants from "../../constants/Constants";
 
 function LandingPage() {
-  const [firstFocus, setFirstFocus] = React.useState(false);
-  const [lastFocus, setLastFocus] = React.useState(false);
-  const [collapseOpen, setCollapseOpen] = React.useState(false);
-  React.useEffect(() => {
-    document.body.classList.add("landing-page");
-    document.body.classList.add("sidebar-collapse");
-    document.documentElement.classList.remove("nav-open");
-    window.scrollTo(0, 0);
-    document.body.scrollTop = 0;
-    return function cleanup() {
-      document.body.classList.remove("landing-page");
-      document.body.classList.remove("sidebar-collapse");
-    };
-  }, []);
   return (
     <>
       <div className="wrapper">
