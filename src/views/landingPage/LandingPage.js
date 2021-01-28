7480e9d8b867556a674945b608d17ea8a9cebebd
Prankul Singh <prankulsinghps@gmail.com>
2021-01-28 22:34:22 +0530

    navbar fix (#1)
    Co-authored-by: Prankul Singh <prankul.singh@airtel.com>

--- src/views/landingPage/LandingPage.js
+++ src/views/landingPage/LandingPage.js
@@ -24,17 +24,6 @@ function LandingPage() {
   const [firstFocus, setFirstFocus] = React.useState(false);
   const [lastFocus, setLastFocus] = React.useState(false);
   const [collapseOpen, setCollapseOpen] = React.useState(false);
-  React.useEffect(() => {
-    document.body.classList.add("landing-page");
-    document.body.classList.add("sidebar-collapse");
-    document.documentElement.classList.remove("nav-open");
-    window.scrollTo(0, 0);
-    document.body.scrollTop = 0;
-    return function cleanup() {
-      document.body.classList.remove("landing-page");
-      document.body.classList.remove("sidebar-collapse");
-    };
-  }, []);
   return (
     <>
       <div className="wrapper">
--- src/views/vision/Vision.js
+++ src/views/vision/Vision.js
@@ -3,31 +3,35 @@ import { Col, Container } from "reactstrap";
 
 function Vision() {
     return (
-        <div className="section base-page vision-page">
-            <Container>
-                <h2>Vision</h2>
-                <div className="image-container">
-                    <img src={require("assets/img/bg1.jpg")} alt={"Nature, United States"} />
+        <>
+            <div className="wrapper">
+                <div className="section base-page vision-page">
+                    <Container>
+                        <h2>Vision</h2>
+                        <div className="image-container">
+                            <img src={require("assets/img/bg1.jpg")} alt={"Nature, United States"} />
+                        </div>
+                        <p>
+                            I will be the leader of a company that ends up being
+                            worth billions of dollars, because I got the answers. I
+                            understand culture. I am the nucleus. I think that’s a
+                            responsibility that I have, to push possibilities, to show
+                            people, this is the level that things could be at.
+                            I will be the leader of a company that ends up being
+                            worth billions of dollars, because I got the answers. I
+                            understand culture. I am the nucleus. I think that’s a
+                            responsibility that I have, to push possibilities, to show
+                            people, this is the level that things could be at.
+                            I will be the leader of a company that ends up being
+                            worth billions of dollars, because I got the answers. I
+                            understand culture. I am the nucleus. I think that’s a
+                            responsibility that I have, to push possibilities, to show
+                            people, this is the level that things could be at.
+                        </p>
+                    </Container>
                 </div>
-                <p>
-                    I will be the leader of a company that ends up being
-                    worth billions of dollars, because I got the answers. I
-                    understand culture. I am the nucleus. I think that’s a
-                    responsibility that I have, to push possibilities, to show
-                    people, this is the level that things could be at.
-                    I will be the leader of a company that ends up being
-                    worth billions of dollars, because I got the answers. I
-                    understand culture. I am the nucleus. I think that’s a
-                    responsibility that I have, to push possibilities, to show
-                    people, this is the level that things could be at.
-                    I will be the leader of a company that ends up being
-                    worth billions of dollars, because I got the answers. I
-                    understand culture. I am the nucleus. I think that’s a
-                    responsibility that I have, to push possibilities, to show
-                    people, this is the level that things could be at.
-                </p>
-            </Container>
-        </div>
+            </div>
+        </>
     );
 }
 
